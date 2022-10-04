8c241b97e3b6f3a68e8f7810986801ff8178689c
Danilo Augusto <danilloagt@gmail.com>
2022-10-04 04:14:37 -0300

    Incio versao 2.0

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// Navigation bar effects on scroll
-window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
